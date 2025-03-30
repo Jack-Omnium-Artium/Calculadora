10e9cf396cd7fe875662e0d3035a7ef382f17bff
Jack-Omnium-Artium <152470190+Jack-Omnium-Artium@users.noreply.github.com>
2025-03-30 00:09:55 -0700

    Update script.js
    Parentesis function has been modified for better botton usability

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function suma(simbolo){
      numeroactual = operacionesjava.innerText; // Obtener el contenido actual del div
 
             // Verificar si el último carácter es un operador '+'
-            if (numeroactual.endsWith('+')|| numeroactual.endsWith('00')|| numeroactual.endsWith('-')|| numeroactual.endsWith('/')|| numeroactual.endsWith('*')) {
+            if (numeroactual.endsWith('+')|| numeroactual.endsWith('-')|| numeroactual.endsWith('/')|| numeroactual.endsWith('*')|| numeroactual.endsWith('.')) {
                 return; // Si ya termina con un '+', no hacer nada
             }
 
@@ -16,67 +16,103 @@ function suma(simbolo){
         }
 
 function resta(simbolo){
-     numeroactual = operacionesjava.innerText; // Obtener el contenido actual del div
+     numeroactual = operacionesjava.innerText; 
 
-            // Verificar si el último carácter es un operador '+'
-            if (numeroactual.endsWith('+')|| numeroactual.endsWith('00')|| numeroactual.endsWith('-')|| numeroactual.endsWith('/')|| numeroactual.endsWith('*')) {
-                return; // Si ya termina con un '-', no hacer nada
+            if (numeroactual.endsWith('+')|| numeroactual.endsWith('-')|| numeroactual.endsWith('/')|| numeroactual.endsWith('*')|| numeroactual.endsWith('.')) {
+                return; 
             }
 
-            // Concatenar el símbolo al contenido actual
-            operacionesjava.innerText = numeroactual + simbolo; // Añadir espacio antes del operador
+            operacionesjava.innerText = numeroactual + simbolo; 
             
         }
 
 function multiplicar(simbolo){
-     numeroactual = operacionesjava.innerText; // Obtener el contenido actual del div
+     numeroactual = operacionesjava.innerText; 
 
-            // Verificar si el último carácter es un operador '+'
-            if (numeroactual.endsWith('+')|| numeroactual.endsWith('00')|| numeroactual.endsWith('-')|| numeroactual.endsWith('/')|| numeroactual.endsWith('*')) {
-                return; // Si ya termina con un '-', no hacer nada
+            if (numeroactual.endsWith('+')|| numeroactual.endsWith('-')|| numeroactual.endsWith('/')|| numeroactual.endsWith('*')|| numeroactual.endsWith('.')) {
+                return; 
             }
 
-            // Concatenar el símbolo al contenido actual
-            operacionesjava.innerText = numeroactual + simbolo; // Añadir espacio antes del operador
+            operacionesjava.innerText = numeroactual + simbolo; 
             
         }
 
 function dividir(simbolo){
-     numeroactual = operacionesjava.innerText; // Obtener el contenido actual del div
+     numeroactual = operacionesjava.innerText; 
 
-            // Verificar si el último carácter es un operador '+'
-            if (numeroactual.endsWith('+')|| numeroactual.endsWith('00')|| numeroactual.endsWith('-')|| numeroactual.endsWith('/')|| numeroactual.endsWith('*')) {
-                return; // Si ya termina con un '-', no hacer nada
+            
+            if (numeroactual.endsWith('+')|| numeroactual.endsWith('-')|| numeroactual.endsWith('/')|| numeroactual.endsWith('*')|| numeroactual.endsWith('.')) {
+                return; 
             }
 
-            // Concatenar el símbolo al contenido actual
-            operacionesjava.innerText = numeroactual + simbolo; // Añadir espacio antes del operador
+            operacionesjava.innerText = numeroactual + simbolo; 
             
         }
 
+function punto(simbolo){
+     numeroactual = operacionesjava.innerText; 
 
+            
+            if (numeroactual.endsWith('+')|| numeroactual.endsWith('-')|| numeroactual.endsWith('/')|| numeroactual.endsWith('*')|| numeroactual.endsWith('-')|| numeroactual.endsWith('/')|| numeroactual.endsWith('.')) {
+                return; 
+            }
+            
+            operacionesjava.innerText = numeroactual + simbolo; 
+        }
+
+function parentesis(){
+    numeroactual = operacionesjava.innerText;
+    const cuentabierto = (operacionesjava.innerText.match(/\(/g) || []).length;
+    const cuentacerrado =(operacionesjava.innerText.match(/\)/g) || []).length;
+   
+
+    operacionesjava.innerText = numeroactual + "(";
+
+    if (numeroactual === "0"){
+        operacionesjava.innerText = "(";
+    }else {
+         operacionesjava.innerText = numeroactual + "*" + "(";
+    }
+
+    if (numeroactual.endsWith('+')|| numeroactual.endsWith('-')|| numeroactual.endsWith('/')|| numeroactual.endsWith('*')|| numeroactual.endsWith('-')|| numeroactual.endsWith('/')|| numeroactual.endsWith('.')|| numeroactual.endsWith('(')){
+        operacionesjava.innerText = numeroactual + "(";
+    }
+    if (numeroactual.includes('(') && /\d$/.test(numeroactual) && cuentabierto > cuentacerrado) {
+         operacionesjava.innerText = numeroactual + ")";
+    } 
+        
+     
+
+}
 
 
 
 
 
 function presionar(numero) {
+
     numeroactual = operacionesjava.innerText; //Con esta linea se actualiza el valor dentro de java, porque si no java sigue pensando que el valor es 0, aun que cambie en el html.
     if (numeroactual === "0"){
         operacionesjava.innerText = numero;
     }else {
          operacionesjava.innerText = numeroactual + numero;
     }
-    if (numeroactual.length >= 15){
+    if (numeroactual.length >= 30){
         operacionesjava.innerText = numeroactual;
     }
     // Verificar si el contenido termina con '+0', y evitar agregar más
     if (numeroactual.endsWith("+0")|| numeroactual.endsWith('-0')|| numeroactual.endsWith('/0')|| numeroactual.endsWith('*0')){
         operacionesjava.innerText = numeroactual;// Restaurar el valor si es inválido
-        return;
+    }
+    if(numeroactual.endsWith(")")){
+        operacionesjava.innerText = numeroactual + "*" + numero;
     }
    
 }
+
+
+   
+
 
 
 
@@ -87,29 +123,14 @@ function limpiar(){
 
 function ejecutaroperacion(){
     numeroactual = operacionesjava.innerText;
-    let operacionejecutada = eval(numeroactual);
+    const operacionejecutada = eval(numeroactual);
+
+    
     respuestaoperacion.innerText = operacionejecutada;
+
+  
 }
 
-
-
-
-
-
-
-
-//function presionar(numero) {
- //   let numeroactual = document.getElementById("Operaciones").innerText; //Al colocar el metodo "innerText" en esta linea condicionas a que el elemento selecionado sea lo que esta detro de la etiqueta y no la etiqueta completa"
- //   if (numeroactual == "0"){
-//        document.getElementById("Operaciones").innerText = numero;
- //   }else {
-   //      document.getElementById("Operaciones").innerText = numeroactual + numero;
- //   }
-//    if (numeroactual.length > 9){
-  //      document.getElementById("Operaciones").innerText = numeroactual;
- //   }
- 
-//}
 
 
 
